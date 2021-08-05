369924c9e2ed6dc04323bcf2576de9d8aadc5a54
yavuz.basar <57456548+yavuzbasar@users.noreply.github.com>
2021-08-04 23:28:43 -0400

    Add files via upload

---
+++ Capstone Project_Week 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Coursera Capstone
+# 
+# The aim of this notebook is to complete the IBM Data Science Coursera Capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[2]:
+
+
+print('Thank You')
+
